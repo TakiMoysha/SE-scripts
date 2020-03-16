f882796c9d4182fc8ba9b78bd1b7e0534678632f
TakiMoysha <mpwema782@gmail.com>
2020-03-17 01:05:59 +0300

    Init commit

---
+++ SpaceEngineers/LCDPlaceInContainer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using VRageMath;
+using VRage.Game;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.Game.EntityComponents;
+using VRage.Game.Components;
+using VRage.Collections;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ModAPI.Ingame;
+using SpaceEngineers.Game.ModAPI.Ingame;
+
+public sealed class LCDPlaceInContainer : MyGridProgram
+{
+    public LCDPlaceInContainer()
+    { }
+
+    public void Main(string args)
+    {
+    }
+
+    public void Save()
+    { }
+    
+}
